beef00b4e7b14afa21169c8617fe3d28646c5660
cassiestephen <cassie.v.stephen@gmail.com>
2023-05-15 20:35:22 -0700

    Main Scene re-done to implement single laser rotating back and forth along edge. Very basic circle added to represent machine that outputs laser

--- project/oceans14.js
+++ project/oceans14.js
@@ -22,7 +22,7 @@ class Line extends Shape {
     constructor() {
         super("position", "color");
         this.arrays.position = Vector3.cast(
-            [-12, 0, 0], [12, 0, 0]
+            [-15, 0, 0], [15, 0, 0]
         );
         this.arrays.color = [
             vec4(0, 1, 0, 1), vec4(0, 1, 0, 1)
@@ -31,7 +31,7 @@ class Line extends Shape {
     }
 }
 
-class Path extends Shape {
+class LaserBox extends Shape {
 
 }
 
@@ -48,6 +48,7 @@ export class Oceans14 extends Scene {
             circle: new defs.Regular_2D_Polygon(1, 15),
             line: new Line(),
             square: new defs.Square(),
+            laser_box: new defs.Subdivision_Sphere(4),
         };
 
         // *** Materials
@@ -64,16 +65,17 @@ export class Oceans14 extends Scene {
         }
         // rotated camera to see between lasers better
         this.initial_camera_location = Mat4.look_at(vec3(0, 10, 20), vec3(0, 0, 0), vec3(0, 1, 0));
-        this.initial_camera_location = this.initial_camera_location.times(Mat4.rotation(-1*Math.PI/12, 1, 0, 0));
+       // this.initial_camera_location = this.initial_camera_location.times(Mat4.rotation(-1*Math.PI/12, 1, 0, 0));
 
 
 
         this.game_started = true;
-        // create initial array of lasers, with a random value for y-value and tilt
-        // inside display, number of them that get rendered will have to do with time!
+        // generate info about lasers for the game
+        this.laser_1_location = Math.random();
+        this.laser_1_max_angle = Math.random();
+        this.laser_2_location = Math.random();
+        this.laser_2_max_angle = Math.random();
 
-        // once all the lasers in the initial array get used up, start adding more
-        // delete lasers from array once they have reached the end?
 
 
 
@@ -103,23 +105,34 @@ export class Oceans14 extends Scene {
     display(context, program_state) {
         // display():  Called once per frame of animation.
         // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
+            // Home screen
+        if (!context.scratchpad.controls) {
+            this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
+            // Define the global camera and projection matrices, which are stored in program_state.
+            program_state.set_camera(this.initial_camera_location);
+        }
+
+        program_state.projection_transform = Mat4.perspective(
+            Math.PI / 4, context.width / context.height, .1, 1000);
+
+        // TODO: Lighting (Requirement 2)
+        const light_position = vec4(0, 5, 5, 1);
+        // The parameters of the Light are: position, color, size
+        program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
+
+        const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
+        const t_seconds = program_state.animation_time;
+        const yellow = hex_color("#fac91a");
+        let model_transform = Mat4.identity();
+        let model_transform_background = Mat4.identity();
+        let model_transform_lasers = Mat4.identity();
+        let model_transform_laser_box = Mat4.identity();
+
+
+
         if (this.game_started === false)
         {
-            // Home screen
-            if (!context.scratchpad.controls) {
-                this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
-                // Define the global camera and projection matrices, which are stored in program_state.
-                program_state.set_camera(this.initial_camera_location);
-            }
-
-            program_state.projection_transform = Mat4.perspective(
-                Math.PI / 4, context.width / context.height, .1, 1000);
-
-            // TODO: Lighting (Requirement 2)
-            const light_position = vec4(0, 5, 5, 1);
-            // The parameters of the Light are: position, color, size
-            program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
-
+            // display a loading screen
 
 
 
@@ -127,270 +140,36 @@ export class Oceans14 extends Scene {
         }
         else
         {
-            // Game running
 
-            //lasers
-
-            if (!context.scratchpad.controls) {
-                this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
-                // Define the global camera and projection matrices, which are stored in program_state.
-                program_state.set_camera(this.initial_camera_location);
-            }
-
-            program_state.projection_transform = Mat4.perspective(
-                Math.PI / 4, context.width / context.height, .1, 1000);
-
-            // TODO: Lighting (Requirement 2)
-            const light_position = vec4(0, 5, 5, 1);
-            // The parameters of the Light are: position, color, size
-            program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
-
-            const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
-            const t_seconds = program_state.animation_time;
-            const yellow = hex_color("#fac91a");
-            let model_transform = Mat4.identity();
-            let model_transform_background = Mat4.identity();
-            let model_transform_lasers = Mat4.identity();
+            // setup lasers
+            // 2 lasers at random spots along sides of view-one on each side, and random spots must be "near" middle?
+            // will be "coming out" of laser generator devices
+            // u have to dodge the 2 lasers
+            // maybe we could have an easy/medium/hard mode that decides the
+            // speeds of the lasers/number of lasers and length of lasers?
 
 
 
-            // setup: location  along z-axis where laser gets generated
-            model_transform_lasers = model_transform_lasers.times(Mat4.translation(0, 0, -20));
+
+            // rotation angle- go from pi/1.5 to -pi/2 for laser on the right hand side of screen
+            let sin_laser_1 =  Math.sin(t);
 
 
-            // location of laser in terms of height
-            // should be a random y-value between -1 and 7
-            let random_amount = Math.random();
-            let linear_combo = (-1 * random_amount) + (7 * (1 - random_amount));
-            model_transform_lasers = model_transform_lasers.times(Mat4.translation(0, 4, 0));
-
-
-            // make laser move towards you
-            model_transform_lasers = model_transform_lasers.times(Mat4.translation(0, 0, 4 * t));
-
-            // tilt of laser
-            // should be a random value between -pi/8 and pi/8, with the more-than-occasional angle = 0
-            model_transform_lasers = model_transform_lasers.times(Mat4.rotation(-1 * Math.PI / 8, 0, 0, 1));
-
-            // draw new laser every 2 seconds
-
+            // // make laser move towards you
+            model_transform_lasers = model_transform_lasers.times(Mat4.translation(15, 0, 0));
+            model_transform_lasers = model_transform_lasers.times(Mat4.rotation(sin_laser_1, 0, 0, 1));
+            model_transform_lasers = model_transform_lasers.times(Mat4.translation(-15, 0, 0));
             this.shapes.line.draw(context, program_state, model_transform_lasers, this.materials.laser, "LINES");
 
-
-            // draw a square as a "ground" for the game
-            // scaled to be 5x5 and moved down to -2 units below origin
-            // rotated so that its flat along "ground"
+            model_transform_laser_box = model_transform_laser_box.times(Mat4.translation(16, 0, 0));
+            model_transform_laser_box = model_transform_laser_box.times(Mat4.scale(0.8, 0.8, 0.8));
+            this.shapes.laser_box.draw(context, program_state, model_transform_laser_box, this.materials.test.override({color: yellow}));
             model_transform = model_transform.times(Mat4.translation(0, -2, 0));
-            model_transform = model_transform.times(Mat4.scale(10, 10, 10));
-            model_transform = model_transform.times(Mat4.rotation(Math.PI / 2, 1, 0, 0));
-            this.shapes.square.draw(context, program_state, model_transform, this.materials.test.override({color: yellow}));
-
-            // draw more squares next to it to create a path along the "ground"
-            model_transform = model_transform.times(Mat4.translation(0, -2, 0));
-            this.shapes.square.draw(context, program_state, model_transform, this.materials.test.override({color: yellow}));
-            model_transform = model_transform.times(Mat4.translation(0, -2, 0));
-            this.shapes.square.draw(context, program_state, model_transform, this.materials.test.override({color: yellow}));
-            model_transform = model_transform.times(Mat4.translation(0, -2, 0));
-            this.shapes.square.draw(context, program_state, model_transform, this.materials.test.override({color: yellow}));
-            model_transform = model_transform.times(Mat4.translation(0, -2, 0));
-            this.shapes.square.draw(context, program_state, model_transform, this.materials.test.override({color: yellow}));
 
 
-            // draw background
-            // model_transform_background = model_transform_background.times(Mat4.scale(300, 300, 1));
-            // model_transform_background = model_transform_background.times(Mat4.translation(0, 0, -900));
-            // this.shapes.square.draw(context, program_state, model_transform_background, this.materials.texture);
+
         }
 
     }
 }
-/*
-class Gouraud_Shader extends Shader {
-    // This is a Shader using Phong_Shader as template
-    // TODO: Modify the glsl coder here to create a Gouraud Shader (Planet 2)
-
-    constructor(num_lights = 2) {
-        super();
-        this.num_lights = num_lights;
-    }
-
-    shared_glsl_code() {
-        // ********* SHARED CODE, INCLUDED IN BOTH SHADERS *********
-        return `
-        precision mediump float;
-        const int N_LIGHTS = ` + this.num_lights + `;
-        uniform float ambient, diffusivity, specularity, smoothness;
-        uniform vec4 light_positions_or_vectors[N_LIGHTS], light_colors[N_LIGHTS];
-        uniform float light_attenuation_factors[N_LIGHTS];
-        uniform vec4 shape_color;
-        uniform vec3 squared_scale, camera_center;
-
-        // Specifier "varying" means a variable's final value will be passed from the vertex shader
-        // on to the next phase (fragment shader), then interpolated per-fragment, weighted by the
-        // pixel fragment's proximity to each of the 3 vertices (barycentric interpolation).
-        varying vec3 N, vertex_worldspace;
-        // ***** PHONG SHADING HAPPENS HERE: *****
-        vec3 phong_model_lights( vec3 N, vec3 vertex_worldspace ){
-            // phong_model_lights():  Add up the lights' contributions.
-            vec3 E = normalize( camera_center - vertex_worldspace );
-            vec3 result = vec3( 0.0 );
-            for(int i = 0; i < N_LIGHTS; i++){
-                // Lights store homogeneous coords - either a position or vector.  If w is 0, the
-                // light will appear directional (uniform direction from all points), and we
-                // simply obtain a vector towards the light by directly using the stored value.
-                // Otherwise if w is 1 it will appear as a point light -- compute the vector to
-                // the point light's location from the current surface point.  In either case,
-                // fade (attenuate) the light as the vector needed to reach it gets longer.
-                vec3 surface_to_light_vector = light_positions_or_vectors[i].xyz -
-                                               light_positions_or_vectors[i].w * vertex_worldspace;
-                float distance_to_light = length( surface_to_light_vector );
-
-                vec3 L = normalize( surface_to_light_vector );
-                vec3 H = normalize( L + E );
-                // Compute the diffuse and specular components from the Phong
-                // Reflection Model, using Blinn's "halfway vector" method:
-                float diffuse  =      max( dot( N, L ), 0.0 );
-                float specular = pow( max( dot( N, H ), 0.0 ), smoothness );
-                float attenuation = 1.0 / (1.0 + light_attenuation_factors[i] * distance_to_light * distance_to_light );
-
-                vec3 light_contribution = shape_color.xyz * light_colors[i].xyz * diffusivity * diffuse
-                                                          + light_colors[i].xyz * specularity * specular;
-                result += attenuation * light_contribution;
-            }
-            return result;
-        } `;
-    }
-
-    vertex_glsl_code() {
-        // ********* VERTEX SHADER *********
-        return this.shared_glsl_code() + `
-            attribute vec3 position, normal;
-            // Position is expressed in object coordinates.
-
-            uniform mat4 model_transform;
-            uniform mat4 projection_camera_model_transform;
-
-            void main(){
-                // The vertex's final resting place (in NDCS):
-                gl_Position = projection_camera_model_transform * vec4( position, 1.0 );
-                // The final normal vector in screen space.
-                N = normalize( mat3( model_transform ) * normal / squared_scale);
-                vertex_worldspace = ( model_transform * vec4( position, 1.0 ) ).xyz;
-            } `;
-    }
-
-    fragment_glsl_code() {
-        // ********* FRAGMENT SHADER *********
-        // A fragment is a pixel that's overlapped by the current triangle.
-        // Fragments affect the final image or get discarded due to depth.
-        return this.shared_glsl_code() + `
-            void main(){
-                // Compute an initial (ambient) color:
-                gl_FragColor = vec4( shape_color.xyz * ambient, shape_color.w );
-                // Compute the final color with contributions from lights:
-                gl_FragColor.xyz += phong_model_lights( normalize( N ), vertex_worldspace );
-            } `;
-    }
-
-    send_material(gl, gpu, material) {
-        // send_material(): Send the desired shape-wide material qualities to the
-        // graphics card, where they will tweak the Phong lighting formula.
-        gl.uniform4fv(gpu.shape_color, material.color);
-        gl.uniform1f(gpu.ambient, material.ambient);
-        gl.uniform1f(gpu.diffusivity, material.diffusivity);
-        gl.uniform1f(gpu.specularity, material.specularity);
-        gl.uniform1f(gpu.smoothness, material.smoothness);
-    }
-
-    send_gpu_state(gl, gpu, gpu_state, model_transform) {
-        // send_gpu_state():  Send the state of our whole drawing context to the GPU.
-        const O = vec4(0, 0, 0, 1), camera_center = gpu_state.camera_transform.times(O).to3();
-        gl.uniform3fv(gpu.camera_center, camera_center);
-        // Use the squared scale trick from "Eric's blog" instead of inverse transpose matrix:
-        const squared_scale = model_transform.reduce(
-            (acc, r) => {
-                return acc.plus(vec4(...r).times_pairwise(r))
-            }, vec4(0, 0, 0, 0)).to3();
-        gl.uniform3fv(gpu.squared_scale, squared_scale);
-        // Send the current matrices to the shader.  Go ahead and pre-compute
-        // the products we'll need of the of the three special matrices and just
-        // cache and send those.  They will be the same throughout this draw
-        // call, and thus across each instance of the vertex shader.
-        // Transpose them since the GPU expects matrices as column-major arrays.
-        const PCM = gpu_state.projection_transform.times(gpu_state.camera_inverse).times(model_transform);
-        gl.uniformMatrix4fv(gpu.model_transform, false, Matrix.flatten_2D_to_1D(model_transform.transposed()));
-        gl.uniformMatrix4fv(gpu.projection_camera_model_transform, false, Matrix.flatten_2D_to_1D(PCM.transposed()));
-
-        // Omitting lights will show only the material color, scaled by the ambient term:
-        if (!gpu_state.lights.length)
-            return;
-
-        const light_positions_flattened = [], light_colors_flattened = [];
-        for (let i = 0; i < 4 * gpu_state.lights.length; i++) {
-            light_positions_flattened.push(gpu_state.lights[Math.floor(i / 4)].position[i % 4]);
-            light_colors_flattened.push(gpu_state.lights[Math.floor(i / 4)].color[i % 4]);
-        }
-        gl.uniform4fv(gpu.light_positions_or_vectors, light_positions_flattened);
-        gl.uniform4fv(gpu.light_colors, light_colors_flattened);
-        gl.uniform1fv(gpu.light_attenuation_factors, gpu_state.lights.map(l => l.attenuation));
-    }
-
-    update_GPU(context, gpu_addresses, gpu_state, model_transform, material) {
-        // update_GPU(): Define how to synchronize our JavaScript's variables to the GPU's.  This is where the shader
-        // recieves ALL of its inputs.  Every value the GPU wants is divided into two categories:  Values that belong
-        // to individual objects being drawn (which we call "Material") and values belonging to the whole scene or
-        // program (which we call the "Program_State").  Send both a material and a program state to the shaders
-        // within this function, one data field at a time, to fully initialize the shader for a draw.
-
-        // Fill in any missing fields in the Material object with custom defaults for this shader:
-        const defaults = {color: color(0, 0, 0, 1), ambient: 0, diffusivity: 1, specularity: 1, smoothness: 40};
-        material = Object.assign({}, defaults, material);
-
-        this.send_material(context, gpu_addresses, material);
-        this.send_gpu_state(context, gpu_addresses, gpu_state, model_transform);
-    }
-}
-
-class Ring_Shader extends Shader {
-    update_GPU(context, gpu_addresses, graphics_state, model_transform, material) {
-        // update_GPU():  Defining how to synchronize our JavaScript's variables to the GPU's:
-        const [P, C, M] = [graphics_state.projection_transform, graphics_state.camera_inverse, model_transform],
-            PCM = P.times(C).times(M);
-        context.uniformMatrix4fv(gpu_addresses.model_transform, false, Matrix.flatten_2D_to_1D(model_transform.transposed()));
-        context.uniformMatrix4fv(gpu_addresses.projection_camera_model_transform, false,
-            Matrix.flatten_2D_to_1D(PCM.transposed()));
-    }
-
-    shared_glsl_code() {
-        // ********* SHARED CODE, INCLUDED IN BOTH SHADERS *********
-        return `
-        precision mediump float;
-        varying vec4 point_position;
-        varying vec4 center;
-        `;
-    }
-
-    vertex_glsl_code() {
-        // ********* VERTEX SHADER *********
-        // TODO:  Complete the main function of the vertex shader (Extra Credit Part II).
-        return this.shared_glsl_code() + `
-        attribute vec3 position;
-        uniform mat4 model_transform;
-        uniform mat4 projection_camera_model_transform;
-
-        void main(){
-
-        }`;
-    }
-
-    fragment_glsl_code() {
-        // ********* FRAGMENT SHADER *********
-        // TODO:  Complete the main function of the fragment shader (Extra Credit Part II).
-        return this.shared_glsl_code() + `
-        void main(){
-
-        }`;
-    }
-}
-*/
 
