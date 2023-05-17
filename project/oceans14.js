91fc8ca83d6740874945daac910be7629086db74
ugathoo <urjagathoo@ucla.edu>
2023-05-16 19:35:10 -0700

    Drone skeleton + rotating, trying to get Windmill going

--- project/oceans14.js
+++ project/oceans14.js
@@ -19,17 +19,17 @@ export class Oceans14 extends Scene {
         this.shapes = {
             torus: new defs.Torus(15, 15),
             torus2: new defs.Torus(3, 15),
-            sphere: new defs.Subdivision_Sphere(4),
+            head: new defs.Subdivision_Sphere(4),
             circle: new defs.Regular_2D_Polygon(1, 15),
-            // TODO:  Fill in as many additional shape instances as needed in this key/value table.
-            //        (Requirement 1)
+            leg: new defs.Cube(),
+            blade: new defs.Windmill(3),
         };
 
         // *** Materials
         this.materials = {
             test: new Material(new defs.Phong_Shader(),
-                {ambient: .4, diffusivity: .6, color: hex_color("#ffffff")}),
-        }
+                {ambient: .4, diffusivity: .6, color: hex_color("#808080")}),
+        };
 
         this.initial_camera_location = Mat4.look_at(vec3(0, 10, 20), vec3(0, 0, 0), vec3(0, 1, 0));
     }
@@ -50,6 +50,7 @@ export class Oceans14 extends Scene {
     display(context, program_state) {
         // display():  Called once per frame of animation.
         // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
+        let model_transform = Mat4.identity();
         if (!context.scratchpad.controls) {
             this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
             // Define the global camera and projection matrices, which are stored in program_state.
@@ -57,22 +58,35 @@ export class Oceans14 extends Scene {
         }
 
         program_state.projection_transform = Mat4.perspective(
-            Math.PI / 4, context.width / context.height, .1, 1000);
+            Math.PI / 4, context.width / context.height, .1, 1000
+        );
+
+        const yellow = hex_color("#fac91a");
+        //
         //
         // // TODO: Create Planets (Requirement 1)
         // // this.shapes.[XXX].draw([XXX]) // <--example
         //
         // // TODO: Lighting (Requirement 2)
-        // const light_position = vec4(0, 5, 5, 1);
+        const light_position = vec4(0, 5, 5, 1);
         // // The parameters of the Light are: position, color, size
-        // program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
+        program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
         //
         // // TODO:  Fill in matrix operations and drawing code to draw the solar system scene (Requirements 3 and 4)
-        // const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
-        // const yellow = hex_color("#fac91a");
+        const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
+        //
         // let model_transform = Mat4.identity();
         //
-        // this.shapes.torus.draw(context, program_state, model_transform, this.materials.test.override({color: yellow}));
+        //this.shapes.torus.draw(context, program_state, model_transform, this.materials.test.override({color: yellow}));
+        //let angle = Math.sin(t) - Math.cos(t);
+        let head_transform = model_transform.times(Mat4.rotation(t, 0,1,0));
+
+        this.shapes.head.draw(context,program_state,head_transform, this.materials.test.override({color: yellow}));
+        let leg_transform1 = head_transform.times(Mat4.scale(1.3,0.2,0.2)).times(Mat4.translation(1.25,0,0));
+        let leg_transform2 = head_transform.times(Mat4.scale(1.3,0.2,0.2)).times(Mat4.translation(-1.25,0,0));
+        this.shapes.leg.draw(context,program_state,leg_transform1,this.materials.test);
+        this.shapes.leg.draw(context,program_state,leg_transform2,this.materials.test);
+        this.shapes.blade.draw(context, program_state, model_transform, this.materials.test);
     }
 }
 /*
